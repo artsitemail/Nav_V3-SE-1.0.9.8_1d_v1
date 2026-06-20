414a216d8cec47172c66b80c14d0b53a5c48ab4b
Artsite <artsitemail@gmail.com>
2026-06-21 00:22:50 +0100

    Update O9001.cpp

--- Marlin/src/gcode/lcd/O9001.cpp
+++ Marlin/src/gcode/lcd/O9001.cpp
@@ -1,10 +1,13 @@
 #include "../../inc/MarlinConfig.h"
-#include "../../MarlinCore.h"
 #include "../gcode.h"
 #include "../parser.h"
 #include "../../lcd/dwin/e3v2/dwin.h"
 #include "../../lcd/marlinui.h"
 
+void GcodeSuite::O9001() {
+  // Função silenciada: Não faz nada, apenas previne erros de compilação
+  return;
+}
 /**
  *O9001: Update Printing Details JOB from OctoPrint in LCD
  *
